b3823ab479f1f4afa963d31382868438e78987be
Kaan Kaya <kaankaya.0707@gmail.com>
2024-12-25 01:10:35 +0300

    SignIn ModelStateExtensions

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using AspNetCoreIdentityApp.Web.Extenisons;
 using AspNetCoreIdentityApp.Web.Models;
 using AspNetCoreIdentityApp.Web.ViewModels;
 using Microsoft.AspNetCore.Identity;
@@ -11,11 +12,13 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         //ýdentity yönetimi için usermanager dependecy yaptýk
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _logger = logger;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -26,6 +29,35 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        //returnUrl kullanýcý giriþ yaptýktan sonra o girmeye çalýþtýðý sayfaya yönlendirmek için kullanacaðýz
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model,string? returnUrl=null)
+        {
+            returnUrl = returnUrl ?? Url.Action("Index", "Home");
+
+           var hasUser = await _userManager.FindByEmailAsync(model.Email);
+            if(hasUser is null)
+            {
+                ModelState.AddModelError(string.Empty, "Email veya Þifre yanlýþ");
+                return View();
+            }
+            var signInresult = await _signInManager.PasswordSignInAsync(hasUser, model.Password, model.RememberMe, false);
+
+            if (signInresult.Succeeded)
+            {
+                return Redirect(returnUrl);
+            }
+
+            ModelState.AddModelErrorList(new List<string>() { "Email yada Þifre yanlýþ" });
+            return View();
+           
+            
         }
 
         [HttpGet]
@@ -52,10 +84,9 @@ namespace AspNetCoreIdentityApp.Web.Controllers
             }
             //eðer hata var ise ýdentity error ile hatalarý içinde döndük.
             //hatayý modelstate imze ekledik,özel birþeye tanýmladýk genel oldugu için ,açýklamasýnda bastýk
-            foreach(IdentityError item in identyResult.Errors)
-            {
-                ModelState.AddModelError(string.Empty,item.Description);
-            }
+
+            ModelState.AddModelErrorList(identyResult.Errors.Select(x=>x.Description).ToList());
+           
             return View();
            
         }
---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignInViewModel
+    {
+        [Required(ErrorMessage ="Email Alanı boş geçilemez")]
+        [EmailAddress(ErrorMessage ="Email Formatı Yanlış")]
+        [Display(Name ="Email :")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Şifre alanı boş geçilemez")]
+        [Display(Name ="Şifre :")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
